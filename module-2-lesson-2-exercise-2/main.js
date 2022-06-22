dbc550ec2ae83623049b6ac5bb3023eec3d99c31
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-06-23 00:19:33 +0200

    Add module 2 lesson 1 exercise 2

---
+++ module-2-lesson-2-exercise-2/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+titleElement.innerHTML = titleElement.innerHTML + " Mundo";
+
+const listElement = document.querySelector(".list");
+const content = '<li>Yaiza</li><li>Laura</li>';
+listElement.innerHTML = content;
+
+const Element = document.querySelector(".password");
+Element.innerHTML = "Mi contraseña es ****";
