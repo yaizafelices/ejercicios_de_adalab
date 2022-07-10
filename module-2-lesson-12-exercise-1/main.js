b881c0861ad0fd9e24dbb260d2678391e2d0a3ae
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-07-11 00:39:25 +0200

    Do exercise 1,2,3 module 2 lesson 12.1 (map)

--- module-2-lesson-12-exercise-1/main.js
+++ module-2-lesson-12-exercise-1/main.js
@@ -11,12 +11,28 @@ console.log(inflatedMarks);
 // Estamos creando una aplicación web, y lo primero que queremos hacer es saludar al usuario por su nombre, ¡como es debido! Tenemos un array con el listado de usuarios de nuestra aplicación const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada']; y queremos conseguir otro array con los saludos, por ejemplo, 'Bienvenida Yolanda'. ¿Podríamos usar map para que nos echase una mano?
 
 const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
-const hello = (name) => "Bienvenida " + name;
-const sayHello = names.map(hello);
+const hello = (name) => "Bienvenida Yolanda" ;
+ const sayHello = names.map(hello);
 
-console.log(sayHello);
+ console.log(sayHello);
 
 // 3. Gracias por confiar en nosotros
 // Seguimos desarrollando nuestra aplicación web que romperá el mercado. Pero antes, queremos agradecer a nuestros usuarios premium (de pago) su ayuda en el saludo de la aplicación. Por tanto, a los usuarios premium queremos saludarles así 'Bienvenida Yolanda. Gracias por confiar en nosotros.', y mantener el saludo simple 'Bienvenida Yolanda' para el resto de usuarios.
 // Vamos a partir de este array con el listado de usuarios que incluye tanto su nombre como si son usuarios premium o no.
 // Tenemos que crear un nuevo array con los saludos. ¿Podremos hacerlo con map?
+
+const users = [
+    { name: "María", isPremium: false },
+    { name: "Lucía", isPremium: true },
+    { name: "Susana", isPremium: true },
+    { name: "Rocío", isPremium: false },
+    { name: "Inmaculada", isPremium: false },
+  ];
+
+const welcome = (users) => {
+    if(users === isPremium){"Bienvenida" +  users.name}
+    else{"Bienvenida Yolanda" ;}}
+
+const sayWelcome = users.map(welcome);
+
+console.log(sayWelcome);
