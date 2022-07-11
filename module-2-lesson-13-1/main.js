46473c80ef0ef4b3672751d4d682c310859ad6e1
Yaiza Soria Felices <yaizafelices@gmail.com>
2022-07-12 00:56:24 +0200

    Add exercise 2.13.1 Fetch

--- module-2-lesson-13-1/main.js
+++ module-2-lesson-13-1/main.js
@@ -1,3 +1,13 @@
-fetch('https://api.rand.fun/number/integer?min=0&max=10000')
+//ESTO ES PARA QUE NOS SALGA UN ICONO DE PIEDRA PAPEL O TIJERA SPOCK O LAGARTO
+
+
+fetch('https://api.rand.fun/games/rockpaperscissorslizardspock')
   .then((response) => response.json())
   .then((data) => (document.body.innerHTML = data.result));
+
+
+//ESTO ES PARA QUE NOS SALGA UN NÚMERO ALEATROIO ENTRE 0-10000
+
+// fetch('https://api.rand.fun/number/integer?min=0&max=10000')
+//   .then((response) => response.json())
+//   .then((data) => (document.body.innerHTML = data.result));
